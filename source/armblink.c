d175230e9a22c1ca3ddcda97057127921f06401f
SamuelB2020 <samuelb2020@student.stlukes.nsw.edu.au>
2016-11-11 17:34:50 +1100

    Extended Delay

--- source/armblink.c
+++ source/armblink.c
@@ -10,12 +10,12 @@ int main(void)
     gpio[LED_GPFSEL] |= (1 << LED_GPFBIT);
     while(1)
     {
-        for(tim = 0; tim < 500000; tim++)
+        for(tim = 0; tim < 1000000; tim++)
             ;
 
         gpio[LED_GPCLR] = (1 << LED_GPIO_BIT);
 
-        for(tim = 0; tim < 500000; tim++)
+        for(tim = 0; tim < 1000000; tim++)
             ;
 
         gpio[LED_GPSET] = (1 << LED_GPIO_BIT);
