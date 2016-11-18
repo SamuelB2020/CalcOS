05343d5b9eae6321cd37117306e5f704680483f2
SamuelB2020 <SamuelBell@Samuels-MacBook-Pro.local>
2016-11-18 14:23:40 +1100

    Added gpioCom to bat file

--- source/build-rpi-2.bat
+++ source/build-rpi-2.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 
-arm-none-eabi-gcc -O0 -DRPI2 -mfpu=neon-vfpv4 -mfloat-abi=soft -march=armv7-a -mtune=cortex-a7 -nostartfiles -g -Wl,-T,lib/rpi.x lib/LedCom.c lib/Timings.c armblink-cstubs.c armblink-cstartup.c armblink.c lib/rpi-systimer.c armc-012-start.S -o kernel.elf
+arm-none-eabi-gcc -O0 -DRPI2 -mfpu=neon-vfpv4 -mfloat-abi=soft -march=armv7-a -mtune=cortex-a7 -nostartfiles -g -Wl,-T,lib/rpi.x lib/LedCom.c lib/Timings.c lib/gpioCom.c armblink-cstubs.c armblink-cstartup.c armblink.c lib/rpi-systimer.c armc-012-start.S -o kernel.elf
  arm-none-eabi-objcopy kernel.elf -O binary kernel7.img
  pause
