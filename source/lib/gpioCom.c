44281d7eb4ebf312b1b9b64ff1dabf8d04b54c1b
SamuelB2020 <SamuelBell@Samuels-MacBook-Pro.local>
2016-11-18 14:14:31 +1100

    Added function definitions

---
+++ source/lib/gpioCom.c
@@ -0,0 +1,12 @@
+extern void GPIORead(int pin){
+
+}
+extern void GPIODigitalWrite(int pin,bool value){
+
+}
+extern void GPIOAnalogWrite(int pin, int value){
+    
+}
+extern void GPIOInit(int pin, bool value){
+    
+}
