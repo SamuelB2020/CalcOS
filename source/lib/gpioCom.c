0b420ce808563ed2eedc5387de8a85df90ecc2da
SamuelB2020 <samuelb2020@student.stlukes.nsw.edu.au>
2016-11-21 08:59:33 +1100

    Added GPIO Support

--- source/lib/gpioCom.c
+++ source/lib/gpioCom.c
@@ -1,3 +1,19 @@
+#include "GPIOCom.h"
+#include <stdint.h>
+#include "gpio.h"
+typedef int bool;
+#define true 1
+#define false 0
+static rpi_gpio_t* rpiGpio = (rpi_gpio_t*)0x3F200000UL;
+
+extern void SetPinMode( rpi_gpio_pin_t gpio, rpi_gpio_alt_function_t func )
+{
+    rpi_reg_rw_t* fsel_reg = &((rpi_reg_rw_t*)rpiGpio)[ gpio / 10 ];
+    rpi_reg_rw_t fsel_copy = *fsel_reg;
+    fsel_copy &= ~( FS_MASK << ( ( gpio % 10 ) * 3 ) );
+    fsel_copy |= (func << ( ( gpio % 10 ) * 3 ) );
+    *fsel_reg = fsel_copy;
+}
 extern void GPIORead(int pin){
 
 }
@@ -8,5 +24,14 @@ extern void GPIOAnalogWrite(int pin, int value){
     
 }
 extern void GPIOInit(int pin, bool value){
+    if(value == 1){
+        SetPinMode(pin, FS_OUTPUT);
+    }
+    if(value == 0){
+        SetPinMode(pin, FS_INPUT);
+    }
+    else{
+        //catch
+    }
     
 }
--- source/lib/rpi-base.h
+++ source/lib/rpi-base.h
@@ -1,4 +1,3 @@
-
 #ifndef RPI_BASE_H
 #define RPI_BASE_H
 
@@ -8,5 +7,5 @@ typedef volatile uint32_t rpi_reg_rw_t;
 typedef volatile const uint32_t rpi_reg_ro_t;
 typedef volatile uint64_t rpi_wreg_rw_t;
 typedef volatile const uint64_t rpi_wreg_ro_t;
-
+typedef volatile uint32_t rpi_reg_wo_t;
 #endif
